b26919aa222f2cef82311db167ab4e236c0aa1f5
Techascendconsulting <techascendconsulting@gmail.com>
2025-08-22 14:59:07 +0100

    Fix syntax error in MeetingSetup.tsx - add missing closing parenthesis

--- src/components/MeetingSetup.tsx
+++ src/components/MeetingSetup.tsx
@@ -384,7 +384,7 @@ export default function MeetingSetup({
                     )}
                   </div>
                 </div>
-              ))}
+              )})}
             </div>
 
             {selectedStage && (
