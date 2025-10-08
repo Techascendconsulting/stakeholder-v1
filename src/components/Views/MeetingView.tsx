68ecd7398ac62115489e68250a294a55ffc590e3
Techascendconsulting <techascendconsulting@gmail.com>
2025-10-08 22:40:15 +0100

    Make chat messages more compact - reduce font size and spacing

--- src/components/Views/MeetingView.tsx
+++ src/components/Views/MeetingView.tsx
@@ -3224,7 +3224,7 @@ ${Array.from(analytics.stakeholderEngagementLevels.entries())
           </div>
 
           {/* Messages */}
-          <div className="flex-1 overflow-y-auto p-6 space-y-4">
+          <div className="flex-1 overflow-y-auto p-4 space-y-3">
             {messages.map((message) => {
               const stakeholder = selectedStakeholders.find(s => s.id === message.speaker)
               const isStakeholderMessage = message.speaker !== 'user' && message.speaker !== 'system'
@@ -3264,7 +3264,7 @@ ${Array.from(analytics.stakeholderEngagementLevels.entries())
                   )}
                   
                   <div
-                    className={`max-w-xs lg:max-w-md px-4 py-3 rounded-lg relative ${
+                    className={`max-w-xs lg:max-w-md px-3 py-2 rounded-lg relative text-sm ${
                       message.speaker === 'user'
                         ? 'bg-blue-600 text-white'
                         : message.speaker === 'system'
